a086421a71930041bb264d256f90c8829416d622
Elsayed Rapaa <Elsayedrapaa7@gmail.com>
2024-09-23 16:13:01 +0300

    Edit

--- components/qr-code.tsx
+++ components/qr-code.tsx
@@ -1,4 +1,6 @@
-import QRCode from "qrcode.react";
+import { useEffect, useState } from "react";
+import QRCode from "qrcode";
+import Image from "next/image";
 
 interface QRCodeAddProps {
   walletAddres: string;
@@ -9,15 +11,34 @@ const QRCodeAdd: React.FC<QRCodeAddProps> = ({
   walletAddres,
   walletCurrency,
 }) => {
+  const [qrCodeUrl, setQrCodeUrl] = useState<string>("");
+
+  useEffect(() => {
+    if (walletAddres) {
+      QRCode.toDataURL(walletAddres)
+        .then((url) => {
+          setQrCodeUrl(url);
+        })
+        .catch((err) => {
+          console.error("Error generating QR Code:", err);
+        });
+    }
+  }, [walletAddres]);
+
   return (
-    <div
-      className="
-        py-2
-      "
-    >
+    <div className="py-2">
       <h3 className="pb-4">Scan code to send funds {walletCurrency}</h3>
       <div className="mx-auto w-fit">
-        <QRCode value={walletAddres} size={256} />
+        {qrCodeUrl ? (
+          <Image
+            src={qrCodeUrl}
+            alt={`QR code for ${walletCurrency} address`}
+            width={256}
+            height={256}
+          />
+        ) : (
+          <p>Generating QR Code...</p>
+        )}
       </div>
     </div>
   );
